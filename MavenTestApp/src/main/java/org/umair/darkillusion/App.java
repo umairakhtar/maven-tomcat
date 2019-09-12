4d262983d5c448e2004e376c02f4b3af10fb0428
Umair <akhtar.u@husky.neu.edu>
2019-09-11 22:06:32 -0700

    First Commit

---
+++ MavenTestApp/src/main/java/org/umair/darkillusion/App.java
@@ -0,0 +1,17 @@
+package org.umair.darkillusion;
+
+import org.slf4j.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+		Logger logger = LoggerFactory.getLogger(App.class);
+		logger.info("Hello World!");
+    }
+}
---
+++ mywebapp/src/main/java/org/umair/darkillusion/MyClass.java
@@ -0,0 +1,11 @@
+package org.umair.darkillusion;
+
+import java.util.*;
+
+public class MyClass
+{
+	public static void main(String[] args)
+	{
+		List<String> stringList = new ArrayList<String>();
+	}
+}
